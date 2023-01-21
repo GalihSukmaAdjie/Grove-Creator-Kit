a62b1936c331c4dfa8a0f51910ead00a66e44585
GalihSukmaAdjie <galih14.gsa@gmail.com>
2023-01-21 10:50:41 +0700

    pemanbahan sensor water

---
+++ Sensors/grove_water_sensor/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#define WATER_SENSOR 2
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(WATER_SENSOR, INPUT);
+}
+
+void loop() {
+  Serial.println(digitalRead(WATER_SENSOR));
+  delay(1000);
+}
