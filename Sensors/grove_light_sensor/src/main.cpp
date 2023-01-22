21f7d2d61189b5d743e51210c6b323ca67041f3a
GalihSukmaAdjie <galih14.gsa@gmail.com>
2023-01-22 12:41:56 +0700

    penambahan light sensor

---
+++ Sensors/grove_light_sensor/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <Grove_LED_Bar.h>
+
+Grove_LED_Bar bar (3, 2, 0);
+
+void setup() {
+  bar.begin();
+  bar.setGreenToRed(true);
+}
+
+void loop() {
+  int value = analogRead(A0);
+  value = map(value, 0, 800, 0, 10);
+
+  bar.setLevel(value);
+  delay(100);
+}
